e2319038e472810594ac58db8ee5ecf7646251ad
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-07-04 23:57:55 +0900

    [Setting] 프로젝트 초기 세팅

---
+++ src/main/java/com/example/kakaoauth/KakaoOAuth2LoginDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakaoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoOAuth2LoginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoOAuth2LoginDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kakaoauth/KakaoOAuth2LoginDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kakaoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoOAuth2LoginDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

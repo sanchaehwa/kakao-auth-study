6eb5b6a0503bbe042d4e564aa035c9bb74cce781
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-07-26 13:06:43 +0900

    [Setting] 환경 변수 설정

--- src/main/java/com/example/kakaoauth/KakaoOAuth2LoginDemoApplication.java
+++ src/main/java/com/example/kakaoauth/KakaoOAuth2LoginDemoApplication.java
@@ -3,11 +3,16 @@ package com.example.kakaoauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class KakaoOAuth2LoginDemoApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(KakaoOAuth2LoginDemoApplication.class, args);
 	}
-
 }
